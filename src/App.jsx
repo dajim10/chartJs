c1f424263f4db2816dfe8519933ca8dd2a787c15
varutcha <varutcha.c@rmutsv.ac.th>
2023-11-10 10:29:54 +0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,48 @@ import BarChart from './components/BarChart_bk'
 const App = () => {
 
     const [counter, setCounter] = useState(0);
+    const [cookie, setCookie] = useState('');
+
+
+
+    // if (cookie === '') {
+    //     setCookie(document.cookie);
+    //     // set cookie timeout
+    //     let d = new Date();
+    //     d.setTime(d.getTime() + (60 * 1000));
+    //     let expires = "expires=" + d.toUTCString();
+    //     document.cookie = `ars=1;${expires};path=/`;
+
+    // }
 
     useEffect(() => {
+
         async function counterStart() {
             let res = await fetch("https://api.rmutsv.ac.th/counter/ars");
             let count = await res.json();
             // document.getElementById("counter").innerHTML = count.counter;
             setCounter(count.counter);
         }
-        counterStart();
+
+        async function counterGet() {
+            let res = await fetch("https://api.rmutsv.ac.th/counter/ars/get");
+            let count = await res.json();
+            // document.getElementById("counter").innerHTML = count.counter;
+            setCounter(count.counter);
+        }
+        // check cookie ars if have cookie not count up 
+        if (document.cookie.indexOf('ars') >= 0) {
+            // console.log('have cookie');
+            counterGet();
+        } else {
+            // console.log('not have cookie');
+            // set cookie timeout
+            let d = new Date();
+            d.setTime(d.getTime() + (3 * 60 * 1000));
+            let expires = "expires=" + d.toUTCString();
+            document.cookie = `ars=1;${expires};path=/`;
+            counterStart();
+        }
     }
         , []);
 
